1f49dbb9c8727cf53b8ac8f1966a998202ca291e
Manoj G M <manojgm1812002@gmail.com>
2025-05-10 00:12:33 +0530

    add program

---
+++ src/Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    public static void main(String[] args)
+    {
+        int a=83;
+        int b=56;
+        int c=a+b;
+        System.out.println("sum of "+ a +" and "+ b + " is "+ c);;
+    }
+}
